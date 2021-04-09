8a5ad59fa0eb9c4e56df9860b5b01eb65fb6565b
Matthew Rowe <matthew.rowe@ibm.com>
2021-04-09 15:29:44 -0400

    App now generates image based on mapping state object in BurgerBuilder.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
 import classes from "./Burger.module.css";
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-    return(
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
+  // line 7: extract all the property names of state.
+  const transformedIngredients = Object.keys(props.ingredients)
+    // map the object into an array of ingredients
+    .map((igKey) => {
+      return [...Array( props.ingredients[igKey] )].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    });
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
