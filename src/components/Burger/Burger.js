1c53f7fd803b29b7b5c881abf0dc84f39f141c7f
Matthew Rowe <matthew.rowe@ibm.com>
2021-04-09 15:57:02 -0400

    app now outputs instructional message if state is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,22 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
   // line 7: extract all the property names of state.
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     // map the object into an array of ingredients
     .map((igKey) => {
       return [...Array( props.ingredients[igKey] )].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
-    });
+    })
+    // flatten arrays
+    .reduce((arr, el) => {
+        // take the element through which we are looping and add it to the array
+        return arr.concat(el) 
+    }, []);
+    if (transformedIngredients.length === 0) {
+         transformedIngredients = <p>Please start adding ingredients</p> 
+    }
+console.log(transformedIngredients);
 
   return (
     <div className={classes.Burger}>
